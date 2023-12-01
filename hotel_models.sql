dc3858f028c47865db2e318d200520d013540294
Yifei Wang <yifeiwang9119@gmail.com>
2023-11-30 20:14:18 -0800

    editing table

--- hotel_models.sql
+++ hotel_models.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 28, 2023 at 06:28 AM
+-- Generation Time: Dec 01, 2023 at 05:13 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -41,13 +41,23 @@ CREATE TABLE `comment` (
 
 CREATE TABLE `hotel` (
   `hotel_id` varchar(20) NOT NULL,
-  `services` varchar(200) DEFAULT NULL,
+  `name` varchar(100) NOT NULL,
+  `details` varchar(1000) NOT NULL,
+  `services` varchar(2000) DEFAULT NULL,
   `rooms` varchar(100) DEFAULT NULL,
   `location` varchar(100) DEFAULT NULL,
   `policies` mediumtext DEFAULT NULL,
   `contact` varchar(50) DEFAULT NULL,
-  `avg_rating` varchar(5) DEFAULT NULL
+  `avg_rating` varchar(5) DEFAULT NULL,
+  `province` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `hotel`
+--
+
+INSERT INTO `hotel` (`hotel_id`, `name`, `details`, `services`, `rooms`, `location`, `policies`, `contact`, `avg_rating`, `province`) VALUES
+('0', 'Super 8 by Wyndham Lethbridge', 'Super 8 is conveniently located less than a mile from Henderson Lake Park and approximately four miles from Lethbridge Airport. The property provides 56 nicely furnished guest rooms situated on two stories. While staying at the inn one can enjoy the complimentary continental breakfast while reading the daily newspaper. There is a heated indoor swimming pool and an exercise gym to suit the customers preferences. The hotel is close to restaurants and various attractions. One can come relax and enjoy the value at the Super 8. All rooms have air-conditioning in-room coffee clock radios modem lines irons and cable TV. Refrigerators and microwaves are available upon request.\r\n', 'Pet Friendly, Fitness Center, Internet Available, Free WiFi, Business Center, Meeting Rooms, Air Conditioning, Television, Breakfast Available, Free Breakfast, Parking Available, Free Parking, Laundry Room, Dry Cleaning', 'King Room, Queen Room, Double Room', '1030 Mayor Magrath Dr S, Lethbridge, AB T1K 2P8', 'Safe Stay: Super 8 by Wyndham Lethbridge has enhanced cleaning and safety measures in place, and follows the Count on Us (Wyndham) cleanliness and sanitization policy. Masks and hand sanitizer are provided to guests, and rooms are disinfected between each stay. Social distancing measures are in place, as well as physical barriers between staff and guests. Cashless payment is available. Each room has a 24-hour gap period between stays, and hotel staff are required to wear masks.', NULL, NULL, 'Alberta');
 
 -- --------------------------------------------------------
 
@@ -109,7 +119,8 @@ ALTER TABLE `comment`
 --
 ALTER TABLE `hotel`
   ADD PRIMARY KEY (`hotel_id`),
-  ADD UNIQUE KEY `hotel_id` (`hotel_id`);
+  ADD UNIQUE KEY `hotel_id` (`hotel_id`),
+  ADD UNIQUE KEY `name` (`name`);
 
 --
 -- Indexes for table `payment`
