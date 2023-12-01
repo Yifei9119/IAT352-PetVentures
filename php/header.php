dd89d49d0d22f9be90057114fcfef151e4c57e51
Yifei Wang <yifeiwang9119@gmail.com>
2023-12-01 15:06:49 -0800

    adding form

--- php/header.php
+++ php/header.php
@@ -1,23 +1,22 @@
 <html>
 
 <head>
-	<title>Pet Ventures</title>
-
+    <title>Pet Ventures</title>
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 
 <body>
-	<table>
-		<tr>
-			<td class="center" style="font-family: sans-serif; font-size:1.25rem">
-            <a href="index.php"><img src="../images/logo.svg" alt="logo"></a>
-					<?php
-					if (isset($_SESSION['valid_user']))
-						echo "<a class=\"nav\"  href=\"logout.php\">Logout</a>";
-					else
-						echo "<a class=\"nav\" href=\"login.php\">Login</a>";
-					?>
-                    |
-                    <a class="nav" href="signup.php">Signup</a>
-			</td>
-		<tr style="background-color:FFFFFF;">
-			<td>
+    <header>
+        <a href="index.php"><img src="../images/logo.svg" alt="logo"></a>
+        <div>
+        <?php
+        if (isset($_SESSION['valid_user']))
+            echo "<a class=\"nav\"  href=\"logout.php\">Logout</a>";
+        else
+            echo "<a class=\"nav\" href=\"login.php\">Login</a>";
+        ?>
+        |
+        <a class="nav" href="signup.php">Signup</a>
+</div>
+
+    </header>
--- php/index.php
+++ php/index.php
@@ -2,5 +2,11 @@
 include_once("../helper/function.php");
 require("header.php");
 
+echo'<form action="users.php" method="GET" style="text-align:center;">
+<input id="search" name="search" type="text" placeholder="Country">
+<input id="date" name="date" type="date">
+<input id="submit" type="submit" value="Search">
+</form>';
+
 require("footer.php");
 ?>
