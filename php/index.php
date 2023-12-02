ea67bdd26157166b53a7e320312c101056557b40
Yifei Wang <yifeiwang9119@gmail.com>
2023-12-02 15:24:20 -0800

    sql update

--- php/index.php
+++ php/index.php
@@ -7,11 +7,9 @@ require("header.php");
 
   
   // Define a query string to select product codes and names from the products table
-  $query_str = "SELECT hotel.hotel_id, hotel.name, hotel.image, MIN(room.price) as price FROM hotel INNER JOIN room ON hotel.hotel_id = room.hotel_id GROUP BY hotel_id";
+  $query_str = "SELECT hotel.hotel_id, hotel.name, hotel.image, hotel.province, MIN(room.price) as price FROM hotel INNER JOIN room ON hotel.hotel_id = room.hotel_id GROUP BY hotel_id";
 
   $res = $db->query($query_str);
-  
-  // Function to create a clickable link for each product model
  
   
   echo "<h2>Pet-Friendly Hotels in Canada</h2>";
@@ -22,7 +20,8 @@ require("header.php");
     echo "<li>";
     // Format each hotel model as a link
     format_hotel_name_as_link($row["hotel_id"], $row["name"],"hoteldetails.php");
-    echo "$". $row['price'] ."<br>";
+    echo "$". $row['price'];
+    echo $row['province']  ."<br>";
     echo'<img width="400" src="../images/hotels/'. $row['image'] .'">';
     echo "</li><br>";
   };
