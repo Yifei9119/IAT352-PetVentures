3ef2be87ae795aec71bddd74f2a7ae3d65d9ceee
Micole <62170592+mwstl@users.noreply.github.com>
2023-12-10 23:10:02 -0800

    Booking Process and History

---
+++ php/confirm_booking.php
@@ -0,0 +1,86 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+// include_once("../helper/function.php");
+require_once("header.php");
+
+$booking_price = "";
+$pet_info = "";
+$options = "";
+$room_id = "";
+$member_id = "";
+
+if (!empty($_REQUEST['bookid'])) { 
+    $booking_id = $_REQUEST['bookid'];
+     
+    $query_str = "SELECT * FROM booking WHERE booking_id = ?";
+    $stmt = $db->prepare($query_str);
+    $stmt->bind_param('s', $booking_id);
+    $stmt->execute();
+    $res = mysqli_stmt_get_result($stmt);
+    
+    // if ($res === false) {
+    //     echo "Error: " . htmlspecialchars($db->error);
+    //     exit;
+    // }
+    // if ($res->num_rows == 0) {
+    //     echo "No hotels found.";
+    //     exit;
+    // }
+
+    while ($row = $res->fetch_assoc()) {
+        $booking_price = $row['total_price'];
+        $pet_info = $row['pet_info'];
+        $options = $row['options'];
+        $room_id = $row['room_id'];
+        $member_id = $row['member_id'];
+    }
+
+    $res->free_result();
+    $db->close();
+}
+
+?>
+
+<div class="booking-confirmation-wrapper">
+    <h1>Booking Successful!</h1>
+    <h2>Details</h2>
+    <table>
+        <tr>
+            <th>Price: </th>
+            <th><?php echo $booking_price; ?></th>
+        </tr>
+        <tr>
+            <th>Pet Info: </th>
+            <!-- <th></th> -->
+        </tr>
+        <tr>
+            <th>Options: </th>
+            <!-- <th></th> -->
+        </tr>
+        </tr>
+        <tr>
+            <th>Room ID: </th>
+            <th><?php echo $room_id; ?></th>
+        </tr>
+        </tr>
+        <tr>
+            <th>Member ID: </th>
+            <th><?php echo $member_id; ?></th>
+        </tr>
+        </tr>
+        <tr>
+            <th>Booking Dates: </th>
+            <!-- <th></th> -->
+        </tr>
+    </table>
+
+    <form action="index.php">
+        <input type="submit" name="submit" value="Return to hotels list">
+    </form>
+</div>
+
+<?php
+include('footer.php');
+?>
---
+++ php/member_bookings.php
@@ -0,0 +1,30 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+// include_once("../helper/function.php");
+require_once("header.php");
+
+// Search for bookings with member id
+$member_id = $_SESSION['valid_user'];
+
+$query_str = "SELECT booking.booking_id, booking.room_id, booking.total_price, room.room_image, room.accommodation FROM booking JOIN room on booking.room_id = room.room_id WHERE member_id = ?";
+$stmt = $db->prepare($query_str);
+$stmt->bind_param('s', $member_id);
+$stmt->execute();
+$res = mysqli_stmt_get_result($stmt);
+?>
+
+<div class="booking-history">
+    <h1>Booked Rooms</h1>
+    <?php
+    while ($row = $res->fetch_assoc()) {
+        format_booking_details($row['booking_id'], $row['room_id'], $row['total_price'], $row['room_image'], $row['accommodation']);
+        // To-do: add in booking times?
+    }
+?>
+</div>
+
+<?php
+include('footer.php');
+?>
