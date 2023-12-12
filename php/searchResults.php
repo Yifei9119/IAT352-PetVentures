ae7e9be4d52c77d5f4e96bc28bc7d4fe00ab6b7e
Yifei9119 <71806425+Yifei9119@users.noreply.github.com>
2023-12-11 21:00:19 -0800

    search result update

--- php/searchResults.php
+++ php/searchResults.php
@@ -13,11 +13,11 @@ if (isset($_POST['search']) && !empty($_POST['search'])) {
        $Name = $_POST['search'];
     //Search query.
         $showTitle=false;
-       $query_str = "SELECT hotel.hotel_id, hotel.name, hotel.image, hotel.province, MIN(room.price) as price FROM hotel INNER JOIN room ON hotel.hotel_id = room.hotel_id WHERE hotel.province LIKE CONCAT('%',?,'%') OR hotel.name LIKE CONCAT('%',?,'%') GROUP BY hotel_id";
+       $query_str = "SELECT hotel.hotel_id, hotel.location, hotel.services, hotel.name, hotel.image, hotel.province, MIN(room.price) as price FROM hotel INNER JOIN room ON hotel.hotel_id = room.hotel_id WHERE hotel.province LIKE CONCAT('%',?,'%') OR hotel.name LIKE CONCAT('%',?,'%') OR hotel.services LIKE CONCAT('%',?,'%') OR hotel.location LIKE CONCAT('%',?,'%') GROUP BY hotel_id";
       
     //prepare and bind
        $stmt = $db->prepare($query_str);
-       $stmt->bind_param('ss',$Name, $Name);
+       $stmt->bind_param('ssss',$Name, $Name, $Name, $Name);
        $stmt->execute();
        $res = mysqli_stmt_get_result($stmt);
     //Query execution
@@ -26,7 +26,7 @@ if (isset($_POST['search']) && !empty($_POST['search'])) {
        // Iterate through each row in the query result
        if ($res->num_rows == 0){
         echo "<h1>No Results Found For '$Name'</h1>";
-        echo "<p style='text-align:center;'>Please search for other places or return to home page</p>";
+        echo "<p style='text-align:center;'>Please search for other keywords or return to home page</p><a class='no-results' href='index.php'>Return to Home</a>";
        }
        else{
        while ($row = $res->fetch_assoc()) {
